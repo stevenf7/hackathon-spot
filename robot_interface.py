8a83c817f8e9d18b5cf16124783f13e8b0078406
Daniel Zou <zoudl2000@gmail.com>
2024-02-10 15:26:52 -0800

    Add robot interface

---
+++ robot_interface.py
@@ -0,0 +1,9 @@
+class RobotInterface:
+    def sit(self):
+        raise NotImplementedError()
+
+    def move(self):
+        raise NotImplementedError()
+
+    def describe_env(self):
+        raise NotImplementedError()
